c1a850d2f695baef2f94d320f8e1cac5917bbfa9
saldaevvladimir <caldaevvlad3@gmail.com>
2025-04-08 18:05:29 +1000

    restructure player script, fix attack animations

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -1,15 +1,32 @@
 extends CharacterBody2D
 
-const SPEED = 250
-# usage not implemented yet
-const HEALTH = 100
-const SWORD1_DMG = 30
-const SWORD2_DMG = 50
+var SPEED = 250
+var health = 100
 const BOW_DMG = 20
 
+const WEAPONS = {
+	"Sword1": {
+		"animation": "SwordAttack1",
+		"damage": 20
+	},
+	"Sword2": {
+		"animation": "SwordAttack2",
+		"damage": 50
+	},
+	"Bow": {
+		"animation": "BowAttack",
+		"damage": 15
+	},
+}
+
 @onready var anim = $AnimatedSprite2D
+var is_attacking = false
+var active_weapon = null
 
 func _physics_process(delta):
+	if is_attacking or !is_alive():
+		return
+	
 	# movement
 	velocity = Vector2.ZERO
 	
@@ -34,12 +51,50 @@ func _physics_process(delta):
 		anim.play("Idle")
 	
 	# attack animations
-	if Input.is_action_just_pressed("Sword1"):
-		anim.play("SwordAttack1")
-	elif Input.is_action_just_pressed("Sword2"):
-		anim.play("SwordAttack2")
-	elif Input.is_action_just_pressed("Bow"):
-		anim.play("BowAttack")
+	if Input.is_action_pressed("Sword1"):
+		attack("Sword1")
+	elif Input.is_action_pressed("Sword2"):
+		attack("Sword2")
+	elif Input.is_action_pressed("Bow"):
+		death()
+		#attack("Bow")
 
-	# other..
+func is_alive():
+	return health > 0
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	print("animation finished")
+	is_attacking = false
+	active_weapon = null
+	
+func get_weapon_damage(weapon):
+	if weapon != null:
+		return WEAPONS[weapon]["damage"]
+	return 0
+	
+func get_weapon_animation(weapon):
+	if weapon:
+		return WEAPONS[weapon]["animation"]
+	return null
+	
+func attack(weapon):
+	if weapon == null:
+		return 
+		
+	is_attacking = true
+	active_weapon = weapon
+	anim.play(get_weapon_animation(weapon))
+		
+func take_damage():
+	health -= 1
+	if health <= 0:
+		death()
+		
+func death():
+	health = 0
+	anim.play("Death")
+	on_death()
+	
+func on_death():
+	print("player dead")
 	
