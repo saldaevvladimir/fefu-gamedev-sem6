2de51dc977d76a88fde69641791acc0e2fa2e6cb
saldaevvladimir <caldaevvlad3@gmail.com>
2025-05-21 19:26:09 +1000

    upd interact system

---
+++ game/scripts/globals.gd
@@ -0,0 +1,14 @@
+# global params and constants
+
+const arrow = preload("res://game/scenes/objects/Arrow.tscn")
+
+const EXISTING_OBJECTS = [
+	"Chest1",
+	"Chest2",
+	"SimpleKey",
+	"MysteryKey",
+	arrow,
+	"Bonus",
+	"Gold",
+	"Shards"
+]
